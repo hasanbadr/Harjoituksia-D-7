d59d490fce5a0233a5fcff888ebbfcbb8f0f07d0
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 07:54:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 1.8) + 32;
+    }
+
+    static void Main()
+    {
+        Console.Write("Syötä Celsius-asteet: ");
+        double celsius = double.Parse(Console.ReadLine());
+
+        double fahrenheit = CelsiusToFahrenheit(celsius);
+        Console.WriteLine($"Fahrenheit: {fahrenheit}");
+    }
+}
